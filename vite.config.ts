31f4e9952a619ede9eb59fc62c23b7a30b2a9cec
Octavio200433 <octaviodobrovits@gmail.com>
2026-07-08 18:51:55 -0300

    correccion coverage CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    // Asegurate de que coincida EXACTAMENTE con el nombre del repositorio en GitHub
-    base: "/TP-agilesG1/",
+    test: {
+        environment: "node",
+        include: ["tests/**/*.test.ts"],
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "lcov"], // 'lcov' es el que genera el archivo necesario para Sonar
+        },
+    },
 });
